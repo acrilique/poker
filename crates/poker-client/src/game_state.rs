a9ea41fc8a89e376ee7952e475f8248dfb12a363
Lluc Simó <lluc.simo@protonmail.com>
2026-03-01 22:44:18 +0100

    chore: add license preamble to all source files

--- crates/poker-client/src/game_state.rs
+++ crates/poker-client/src/game_state.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2026 Lluc Simó Margalef <lluc.simo@protonmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This file is part of acrilique/poker.
+//
+// poker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// poker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with poker. If not, see <https://www.gnu.org/licenses/>.
+
 use std::collections::{HashMap, HashSet, VecDeque};
 
 use poker_core::poker::{Board, Hand, HandRank};
--- crates/poker-client/src/lib.rs
+++ crates/poker-client/src/lib.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2026 Lluc Simó Margalef <lluc.simo@protonmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This file is part of acrilique/poker.
+//
+// poker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// poker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with poker. If not, see <https://www.gnu.org/licenses/>.
+
 pub mod client_controller;
 pub mod game_state;
 pub mod net_client;
